f738f2b68678c63966c4a52e4a7f16f74952333f
Alejandro Arteaga <alejow18@gmail.com>
2021-02-16 23:04:35 -0600

    Portafolio personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagenotfoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
